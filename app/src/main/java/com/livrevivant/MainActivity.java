fe62e8ea2bdb4fa0b52ba1c85f69f7b339cd37c7
BenjaminClt <benjamin.cleton@hotmail.com>
2019-02-15 12:45:02 +0100

    base application

---
+++ app/src/main/java/com/livrevivant/MainActivity.java
@@ -0,0 +1,91 @@
+package com.livrevivant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Frame;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.livrevivant.augmentedImage.AugmentedImageNode;
+import com.livrevivant.common.helpers.SnackbarHelper;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+    private ImageView fitToScanView;
+
+    // Augmented image and its associated center pose anchor, keyed by the augmented image in
+    // the database.
+    private final Map<AugmentedImage, AugmentedImageNode> augmentedImageMap = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (augmentedImageMap.isEmpty()) {
+            fitToScanView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Registered with the Sceneform Scene object, this method is called at the start of each frame.
+     *
+     * @param frameTime - time since last frame.
+     */
+    private void onUpdateFrame(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        // If there is no frame or ARCore is not tracking yet, just return.
+        if (frame == null || frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+            return;
+        }
+
+        Collection<AugmentedImage> updatedAugmentedImages =
+                frame.getUpdatedTrackables(AugmentedImage.class);
+        for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+            switch (augmentedImage.getTrackingState()) {
+                case PAUSED:
+                    // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                    // but not yet tracked.
+                    String text = "Detected Image " + augmentedImage.getIndex();
+                    SnackbarHelper.getInstance().showMessage(this, text);
+                    break;
+
+                case TRACKING:
+                    // Have to switch to UI Thread to update View.
+                    fitToScanView.setVisibility(View.GONE);
+
+                    // Create a new anchor for newly found images.
+                    if (!augmentedImageMap.containsKey(augmentedImage)) {
+                        AugmentedImageNode node = new AugmentedImageNode(this);
+                        node.setImage(augmentedImage);
+                        augmentedImageMap.put(augmentedImage, node);
+                        arFragment.getArSceneView().getScene().addChild(node);
+                    }
+                    break;
+
+                case STOPPED:
+                    augmentedImageMap.remove(augmentedImage);
+                    break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/livrevivant/augmentedImage/AugmentedImageNode.java
@@ -0,0 +1,119 @@
+package com.livrevivant.augmentedImage;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Pose;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.ExternalTexture;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.livrevivant.R;
+
+public class AugmentedImageNode extends AnchorNode {
+
+    private static final String TAG = "AugmentedImageNode";
+
+    // The augmented image represented by this node.
+    private AugmentedImage image;
+
+    private static ModelRenderable videoRenderable;
+    private ExternalTexture texture;
+    private Context context;
+
+    // The color to filter out of the video.
+    private static final Color CHROMA_KEY_COLOR = new Color(0.1843f, 1.0f, 0.098f);
+
+    private static final boolean DISABLE_CHROMA_KEY = true;
+
+    public AugmentedImageNode(Context context) {
+        this.context = context;
+        texture = new ExternalTexture();
+        ModelRenderable.builder()
+                .setSource(context, R.raw.chroma_key_video)
+                .build()
+                .thenAccept(
+                        renderable -> {
+                            videoRenderable = renderable;
+                            renderable.getMaterial().setExternalTexture("videoTexture", texture);
+                            renderable.getMaterial().setFloat4("keyColor", CHROMA_KEY_COLOR);
+                            renderable.getMaterial().setBoolean("disableChromaKey", DISABLE_CHROMA_KEY);
+                        })
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(context, "Unable to load video renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+    }
+
+    /**
+     * Called when the AugmentedImage is detected and should be rendered. A Sceneform node tree is
+     * created based on an Anchor created from the image. The corners are then positioned based on the
+     * extents of the image. There is no need to worry about world coordinates since everything is
+     * relative to the center of the image, which is the parent node of the corners.
+     */
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    public void setImage(AugmentedImage image) {
+        this.image = image;
+        // Create an Android MediaPlayer to capture the video on the external texture's surface.
+        MediaPlayer mediaPlayer = null;
+
+        if (image.getName().contains("image1")) {
+            mediaPlayer = MediaPlayer.create(context, R.raw.small);
+        } else {
+            mediaPlayer = MediaPlayer.create(context, R.raw.sample);
+        }
+
+        mediaPlayer.setSurface(texture.getSurface());
+        mediaPlayer.setLooping(true);
+
+        Pose pose = image.getCenterPose();
+        Pose rotate = Pose.makeRotation(1,0,0,-1);
+        Pose translate = Pose.makeTranslation(0,-image.getExtentZ() / 4, 0);
+
+        pose = pose.compose(rotate).compose(translate);
+
+        // Set the anchor based on the center of the image.
+        setAnchor(image.createAnchor(pose));
+
+        // Create a node to render the video and add it to the anchor.
+        Node videoNode = new Node();
+        videoNode.setParent(this);
+
+        videoNode.setLocalScale( new Vector3(
+                        image.getExtentX() , image.getExtentZ(),  0.5f));
+
+
+
+        // Start playing the video when the first node is placed.
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+
+            // Wait to set the renderable until the first frame of the  video becomes available.
+            // This prevents the renderable from briefly appearing as a black quad before the video
+            // plays.
+            texture
+                    .getSurfaceTexture()
+                    .setOnFrameAvailableListener(
+                            (SurfaceTexture surfaceTexture) -> {
+                                videoNode.setRenderable(videoRenderable);
+                                texture.getSurfaceTexture().setOnFrameAvailableListener(null);
+                            });
+        } else {
+            videoNode.setRenderable(videoRenderable);
+        }
+    }
+
+    public AugmentedImage getImage() {
+        return image;
+    }
+}
